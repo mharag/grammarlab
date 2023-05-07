85d0c7f77a0610dd963e561f09a4ac550c639ab1
Miroslav Harag <miroslav.harag@kiwi.com>
2023-05-07 21:35:41 +0200

    more tests

--- test/reggresion/test_a_a_a.py
+++ test/reggresion/test_a_a_a.py
@@ -3,18 +3,49 @@ from glab.core.config import STRING_DELIMITER
 from glab.examples.cs_aaa import grammar
 
 
-def test_a_a_a(capsys):
-    expected = ["a_a_a",
+def format_expected_result(expected):
+    expected = [STRING_DELIMITER.join(x) for x in expected]
+    expected = "\n".join(expected) + "\n"
+    return expected
+
+
+def test_generate(capsys):
+    expected = format_expected_result(["a_a_a",
         "aa_aa_aa",
         "aaa_aaa_aaa",
         "aaaa_aaaa_aaaa",
         "aaaaa_aaaaa_aaaaa",
         "aaaaaa_aaaaaa_aaaaaa",
-    ]
-    expected = [STRING_DELIMITER.join(x) for x in expected]
-    expected = "\n".join(expected) + "\n"
+    ])
 
     App(grammar).generate(50)
     captured = capsys.readouterr()
-    print(captured.out)
+    assert captured.out == expected
+
+
+def test_generate_exact_depth(capsys):
+    expected = format_expected_result(["aaa_aaa_aaa"])
+
+    App(grammar).generate(11, exact_depth=True)
+    captured = capsys.readouterr()
+    assert captured.out == expected
+
+
+def test_generate_sential_form(capsys):
+    expected = format_expected_result([
+        "a-L-a",
+        "a-F_a",
+        "aa-AR-a"
+    ])
+
+    App(grammar).generate(2, only_sentences=False)
+    captured = capsys.readouterr()
+    assert captured.out == expected
+
+
+def test_generate_start(capsys):
+    expected = format_expected_result(["a_a_a"])
+
+    App(grammar).generate(1, start="a_a-F")
+    captured = capsys.readouterr()
     assert captured.out == expected
